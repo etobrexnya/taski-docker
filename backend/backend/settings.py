c41eb687f62852c9cbb8b5cf263ed121e7e03d30
Arina Gareeva <etobrexnya@gmail.com>
2024-09-28 15:08:33 +0300

    Upd with flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,11 @@
+import os
 from pathlib import Path
 
-import os
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
-
 
 ALLOWED_HOSTS = ['89.169.173.86', '127.0.0.1', 'localhost', 'project1.ddns.net']
 
